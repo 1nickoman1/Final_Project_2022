ebcb971032ad952e025f411df5826f9b6bbdd3ae
Aaron Canchola <1526331@SUHSD.NET>
2022-03-09 14:15:08 -0800

    Second Commit-Files

---
+++ Final/Dev.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 9, 2022
+
+@author: 1526331
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
