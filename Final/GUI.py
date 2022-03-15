1530526d80c1cad09ea58074a409480f8368fd9f
Aaron Canchola <1526331@SUHSD.NET>
2022-03-15 14:08:33 -0700

    Updated the Questions

---
+++ Final/GUI.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 15, 2022
+
+@author: 1526331
+'''
+#imports
+ 
+ 
+ 
+ 
+ 
+ 
+#functions
+
+
+
+
+if __name__ == '__main__':
+    win = 
--- Final/Main.py
+++ Final/Main.py
@@ -1,35 +1,55 @@
-from Final.Dev.Text_questions import multiple_question
+scoreboard = 0 
 
-# def multiple_question(question, maybeanswer1, correctanswer1, correctanswer2, correctmessage, wrongmessage, scoreboard):
-#     '''
-#     Multiple question setup, It will ask for a question, and two maybe answers as well two correct answers and wrong message.
-#     '''
-#     print(question)
-#     maybeanswer1 = input(maybeanswer1)
-#     if maybeanswer1.lower() == correctanswer1 or maybeanswer1.lower() == correctanswer2:
-#         print(correctmessage)
-#         scoreboard += 1
-#     else:
-#         print(wrongmessage)
-#
-#
-# def true_false_question(question, correctanswer1, correctanswer2, correctmessage, wrongmessage, scoreboard):
-#     '''
-#     True and false question setup. It will ask if the question is true or false.
-#     '''
-#     print(question)
-#     maybeanswer1 = input(":")
-#     if maybeanswer1.lower() == correctanswer1 or maybeanswer1.lower() == correctanswer2:
-#         print(correctmessage)
-#         scoreboard += 1
-#     else:
-#         print(wrongmessage)
+
+
+#import
+
+
+
+#function
+def correct_question(score):    
+    if score == "Correct":
+        global scoreboard
+        scoreboard = scoreboard + 1
+        
+def wrong_question(score):
+    if score == "Wrong":
+        global scoreboard
+        scoreboard = scoreboard - 1
+    
+def multiple_question(question, choice, first_correct, second_correct, correct_message, wrong_message):
+    '''
+        Multiple question setup, It will ask for a question, and two maybe answers as well two correct answers and wrong message.
+    '''
+    print(question)
+    choice = input(choice)
+    if choice.lower() == first_correct or choice.lower() == second_correct:
+        print(correct_message)
+        correct_question("Correct")
+    else:
+        print(wrong_message)    
+        wrong_question("Wrong")
+        
+        
+def true_false_question(question, first_correct, second_correct, correct_message, wrong_message):
+    '''
+    True and false question setup. It will ask if the question is true or false.
+    '''
+    print(question)
+    choice = input(":")
+    if choice.lower() == first_correct or choice.lower() == second_correct:
+        print(correct_message)
+        correct_question("Correct")
+    else:
+        print(wrong_message)    
+        wrong_question("Wrong")
+
+
+
 
 if __name__ == '__main__':
 # Defining Score variables
-    x = 0
-    score = x
-    
+
     # Question One 
     question1 = "What is 1 + 1"
     maybeanswer1 = "a)1\nb)2\nc)3\nd)4\n:"
@@ -37,15 +57,16 @@ if __name__ == '__main__':
     correctanswer2 = "2"
     wrongmessage = "Incorrect, 1 + 1 is 2"
     correctmessage = "Test"
-    multiple_question(question1, maybeanswer1, correctanswer1, correctanswer2, correctmessage, wrongmessage, score)
+    multiple_question(question1, maybeanswer1, correctanswer1, correctanswer2, correctmessage, wrongmessage)
 
-# question1_3 = "True or False... The Toronto Maple Leafs have won 13 Stanley Cups?"
-# correctanswer1_3 = "true"
-# correctanswer2_3 = "t"
-# wrongmessage_3 = "Incorrect, 1 + 1 is 2"
-# correctmessage_3 = "Test"
-# true_false_question(question1_3, correctanswer1_3, correctanswer2_3, correctmessage_3, wrongmessage_3, score)
+    question1_3 = "True or False... The Toronto Maple Leafs have won 13 Stanley Cups?"
+    correctanswer1_3 = "true"
+    correctanswer2_3 = "t"
+    wrongmessage_3 = "Incorrect, 1 + 1 is 2"
+    correctmessage_3 = "Test"
+    true_false_question(question1_3, correctanswer1_3, correctanswer2_3, correctmessage_3, wrongmessage_3)
 
 # Total Score
-score = float(x / 5) * 100
-print(x, "out of 5, that is", score, "%")
+
+    Test = float(scoreboard / 5) * 100
+    print(scoreboard, "out of 5, that is", Test, "%")
