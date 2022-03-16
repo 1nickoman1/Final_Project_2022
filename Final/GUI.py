3287253e68a1c04de6b1adfa38e84c13e6072371
Nick <itsyourboyvux@gmail.com>
2022-03-15 21:25:36 -0700

    Fourth Commit-Update ReadME.md

--- Final/GUI.py
+++ Final/GUI.py
@@ -4,16 +4,20 @@ Created on Mar 15, 2022
 @author: 1526331
 '''
 #imports
- 
- 
- 
- 
- 
- 
-#functions
+from tkinter import *
+from tkinter.ttk import *
+from PIL import Image
 
-
+# functions
 
 
 if __name__ == '__main__':
-    win = 
+    win = Tk()
+    win.title("Application")
+    win.iconwindow('')
+    win.geometry('500x500')
+    win.config(bg='white')
+
+    win.mainloop()
+
+
